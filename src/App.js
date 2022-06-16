b35984fd21c6e561e340454cf9d89a1bc8ad7e57
Jules Verdadero <Julesverd@gmail.com>
2022-06-16 08:52:26 -0400

    Pokedex Error in index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Main from './Components/Main';
+import './Components/style.css';
 
 function App() {
-  return null
+  return (
+    <>
+      <Main/>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const Card=()=>{
+    return(
+        <>
+            <div className="card">
+                <h2>1</h2>
+                <img src=".img/charm.png" alt="" />
+                <h2>Charmander</h2>
+            </div>
+        </>
+    )
+}
+export default Card;
